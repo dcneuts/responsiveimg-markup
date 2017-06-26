da597c46377f91384ebeebac05afa7d12809dd50
Derek Neuts <dcneuts@gmail.com>
2017-06-25 17:32:49 -0700

    Used slice method to remove file endings from images

--- JS/script.js
+++ JS/script.js
@@ -2,5 +2,7 @@ const IMAGES = document.querySelectorAll("img");
 
 for (let i = 0; i<IMAGES.length; i++) {
   let imgSrc = IMAGES[i].getAttribute("src");
+  // To remove the file endings from the images for later modification
+  imgSrc = imgSrc.slice(0, -8);
   console.log(imgSrc);
 }
