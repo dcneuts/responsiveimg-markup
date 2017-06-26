a108e5f1f64ff73f76a5b3bb86343c2226d60f0d
Derek Neuts <dcneuts@gmail.com>
2017-06-25 20:00:41 -0700

    Fixed miscellaneous issues and cleaned up code

--- JS/script.js
+++ JS/script.js
@@ -11,21 +11,21 @@ function makeSrcset(imgSrc) {
   let width = 400;
 
   // Loop to grab all 5 image sizes and append to imgSrc a specific output
-  for (let i = 0; i<5; i++) {
+  for (let i = 0; i < 5; i++) {
     markup[i] = imgSrc + "-" + width + ".jpg " + width + "w";
     // Adding another 400 pixels onto the image size
-    width+=400;
+    width += 400;
   }
   // Join used to generate comma-separated list
   return markup.join();
 }
 
-for (let i = 0; i<IMAGES.length; i++) {
+for (let i = 0; i < IMAGES.length; i++) {
   let imgSrc = IMAGES[i].getAttribute("src");
   // To remove the file endings from the images for later modification
   imgSrc = imgSrc.slice(0, -8);
-  //
   let srcset = makeSrcset(imgSrc);
+  
   // Places values in srcset
   IMAGES[i].setAttribute("srcset", srcset);
 
